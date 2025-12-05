4a7bd94b8a6c0577c9afa2be31e26c1b69df0767
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2025-12-05 09:44:48 +0200

    clean and build

--- app/Http/Controllers/MealPlan/MealPlanController.php
+++ app/Http/Controllers/MealPlan/MealPlanController.php
@@ -12,10 +12,17 @@ class MealPlanController extends Controller
         try{
             $user=Autho::user();
             $request->validate([
-                'text'=>'require|string'
+                'text'=>'require|string',
+                'UseOldRecipe'=>'sometime|boolean'
             ]);
             $text=$request->input('text');
-            $result=$this->MealPlanService->GenerateMealByAIForWeak();
+            $useOldRecipe->input('UseOldRecipe');
+            if($useOldRecipe){
+                 $result=$this->MealPlanService->GenerateMealByAIForWeakWithRecipes($text,$user);
+            }else{
+                $result=$this->MealPlanService->GenerateMealByAIForWeak($text,$user);
+            }
+            
             if($result['success']){
                  return ResponseTrait::success($result);
             }
@@ -28,6 +35,38 @@ class MealPlanController extends Controller
        
 
     }
+    public function saveMealsForWeak(Request $request){
+        try{
+            $user=Autho::user();
+            if(!$user){
+                return ResponseTrait::error("is not authorized");
+            }
+    
+            $validated = $request->validate([
+                'household_id' => 'required|exists:households,id',
+                'week_start' => 'required|date',
+                'week_plan' => 'required|array',
+    
+                'week_plan.*.day_of_week' => 'required|integer|min(0)|max(6)',
+                'week_plan.*.slot' => 'required|in:breakfast,lunch,dinner',
+                'week_plan.*.recipe_id' => 'required|exists:recipes,id',
+            ]);
+            $result=$this->MealPlanService->SaveMealPlaneService($validated,$user);
+            return ResponseTrait::success(data:$result);
+        }catch(Exception $e){
+            return ResponseTrait::error($e);
+        }
+
+    }
+    public function getMeals(Request $request){
+        try{
+            $user=Autho::user();
+            
+        }catch(Exception $e){
+            return ResponseTrait::error($e);
+        }
+    }
+
 
     
     
--- app/services/MealPlanService.php
+++ app/services/MealPlanService.php
@@ -23,15 +23,58 @@ class MealPlanService
        }
        throw throw new \Exception( "there is problem");
     }
-    function GenerateMealByAIForWeakWithoutRecipes($text,$user){
+    function GenerateMealByAIForWeakWithoutCheckTheExistRecipes($text,$user){
        return  HelperGenerateMealByAIForWeak($text,$user);
     }
     function GenerateMealByAIForWeakWithRecipes($text,$user){
         $householder_id=$this->GetHouseHolderId($user);
         $recipes=encode(Recipe::where('householder_id',$householder_id)->get());
         return  HelperGenerateMealByAIForWeak($text,$user,$recipes);
+    }
+
+    public function SaveMealPlaneService($data, $user){
+        $admin=Admin::where('user_id',$user->id);
+        $member=HouseHolderMember::where('user_id',$user->id);
+        $householder=Household::where('user_id',$user->id);
+
+        if($admin){
+            throw new \Exception("the admin can not save");
+        }
+        else if($householder){
+            $houseHolder_id=$householder->id;
+        }
+        else if($member){
+            $houseHolder_id=$member->householder_id;
+        }
+
+        $mealPlan=new MealPlan();
+
+        $mealPlane->household_id=$houseHolder_id;
+        $mealPlane->week_start=$data['week_start'];
+        if(!$mealPlane->save()){
+            throw new \Exception("the error in saveing on database ");
+        }
+
+        foreach ($data['week_plan'] as $item) {
+            $mealPlaneItem=new MealPlanItem();
+            $mealPlaneItem->meal_plan_id=$mealPlan->id;
+            $mealPlaneItem->recipe_id=$recipe_id;
+            $mealPlaneItem->day_of_week=$day_of_week;
+            $mealPlaneItem->slot=$slot;
+            if(!$mealplaneItem->save()){
+                throw new \Excption("the error on saveing Item");
+            }
+        }
+        return $mealPlan;
+
+    
 
     }
+
+
+
+
+
 }
 
 
