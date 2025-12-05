c1a07271c16b054eb93179cf96f2259351140145
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2025-12-05 13:28:50 +0200

    build and clean

--- app/Http/Controllers/ShoppingList/ShoppingListController.php
+++ app/Http/Controllers/ShoppingList/ShoppingListController.php
@@ -32,14 +32,31 @@ class ShoppingListController extends Controller
 
 
         }catch(Exception $e){
-            ResponseTrait::error($e);
+           return ResponseTrait::error($e);
         }
 
     }
     public function AddItemForShopList(Request $request){
 
         try{
-
+            $user=Autho::user();
+            if(!$user){
+                return ResponseTrait::error("user is not authenticated");
+            }
+            $data=$request->validate([
+                'shopping_list_id'=>'required|exists:shopping_lists,id',
+                'items' => 'required|array|min:1',
+            
+                'items.*.name' => 'required|string|max:255',
+            
+                'items.*.quantity' => 'nullable|numeric|min:0',
+            
+                'items.*.unit_id' => 'nullable|exists:units,id',
+            
+                'items.*.is_bought' => 'sometimes|boolean'                
+            ]);
+            $result=$this->SHopingListService->addItemsToList($user,$data);
+            return  ResponseTrait::success(data:$result);
         }catch(Exception $e){
             return ResponseTrait::error($e);
         }
@@ -50,6 +67,7 @@ class ShoppingListController extends Controller
             if(!$user){
                 return ResponseTrait::error("the user is authorized");
             }
+            
         }catch(Exception $e){
             ResponseTrait::error($e);
         }
--- app/services/ShopingListService.php
+++ app/services/ShopingListService.php
@@ -1,20 +1,31 @@
 <?php
 
 class ShopingListService{
-
-    function PostShopListFromClientService($validatedata, $user){
-        $admin=Admin::where('user_id',$user->id);
+    public function CheckUserRoleAndGetHouseHolderId($user){
         $householder=Household::where('user_id',$user->id);
         $member=HouseHolderMember::where('userid',$user->id);
-
-        if($admin){
-            throw new Exception("the admin can nt post data");
-        }
         if($householder){
             $houseHolderId=$householder->id;
         }
-        if($member){
+        else if($member){
             $houseHolderId=$member->houseHolder_id;
+        }
+        else{
+            throw \Exception ("the user is not have role that allow him to add items");
+        }
+        return $houseHolderId;
+
+    }
+
+    function PostShopListFromClientService($validatedata, $user){
+        $admin=Admin::where('user_id',$user->id);
+        
+        if($admin){
+            throw new Exception("the admin can nt post data");
+        }
+        $houseHolderId=CheckUserRoleAndGetHouseHolderId($user);
+        if(!$houseHolderId){
+            throw Exception("errorre");
         }
         $shoppingList=new ShoppingList();
         $shoppingList->houseHolder_id=$houseHolderId;
@@ -24,6 +35,7 @@ class ShopingListService{
 
         foreach($validatedata['items'] as $item){
             $shoppListItem=new ShoppingListItem();
+            $shoppListItem->shopping_list_id=$shoppingList->id;
             $shoppListItem->name=$item['name'];
             $shoppListItem->quantity=$item['quantity'];
             $shoppListItem->unit_id=$item['unit_id'];
@@ -36,8 +48,28 @@ class ShopingListService{
         return $shoppingList;
 
     }
+    public function addItemsToList($user,$data){
+        $houseHolderId=CheckUserRoleAndGetHouseHolderId($user);
+        if(!$houseHolderId){
+            throw Exception("errorre");
+        }
+        $addShoppingListItems=[];
+        foreach($data['items'] as $item){
+            $shoppListItem=new ShoppingListItem();
+            $shoppListItem->$data['shopping_list_id'];
+            $shoppListItem->name=$item['name'];
+            $shoppListItem->quantity=$item['quantity'];
+            $shoppListItem->unit_id=$item['unit_id'];
+            $shoppListItem->is_bought=$item['is_bought'];
+            if($shoppingListItem->save()){
+                throw Exception("the eror in save the items ");
+            }
+            $addShoppingListItems=[$shoppListItem];
+        } 
+        return $addShoppingListItems;
+    }
     public function updataShopList(){
-        
+
     }
 }
 
